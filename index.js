c5d55c154c3c9414ab2c2c669185ce4cae7c6ff1
Charles Magalhães <charlesrm29@gmail.com>
2021-07-13 18:44:56 -0300

    adicionado o cors

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import tasksRouter from './routes/tasks.js';
 import { promises as fs } from 'fs';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerDocument } from './doc.js';
+import cors from 'cors';
 
 const { readFile, writeFile } = fs;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/task', tasksRouter);
 
